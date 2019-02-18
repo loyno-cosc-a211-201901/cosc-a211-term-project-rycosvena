1adbc7797595e7812e09a72eb398da61f49e7a2e
rycosvena <rycosvena@users.noreply.github.com>
2019-02-18 13:44:11 -0600

    Evans_ComicBookOrganizer
    Created an do while loop for the inputs. I  changed the if/while statement to an if statement inside the do while and an if statement outside.

--- Final Project Submission/Evans_ComicBookOrganizer.cpp
+++ Final Project Submission/Evans_ComicBookOrganizer.cpp
@@ -57,42 +57,45 @@ int main()
 	cout << "Do you want to input more comics? Y (Yes) or N (No)" << endl;
 	 cin >> userAnswer;
 // I probably need if/ else
-	
-	if (userAnswer == 'Y'){
-		cin.ignore(100,'\n');
-		cout << "Please enter series name." << endl;
-		getline(cin, seriesName);
-		cout << "Please enter issue name, if applicable." << endl;
-		getline(cin, issueName); // Can be considered a subtitle
-		cout << "Please enter author's name." << endl;
-		getline(cin, author);
-		cout << "Please enter the artist's or penciller's name." << endl;
-		getline(cin, artistOrPenciller); // Comics can say either
-		cout << "Please enter colorist's name." << endl;
-		getline(cin, colorist); // The artist usually only draws, not colors
-		cout << "Please enter issue number." << endl;
-		 cin >> issueNumber;
-		cout << "Please enter variant letter." << endl;
-		 cin >> variantLetter;
-		cout << "Please enter the publish date. Separate the month and year with a slash if necessary." << endl;
-		 cin >> publishDate; // string/getline didn't work.
-		cout << "Please enter condition rating. (Use letter notation.)" << endl;
-		 cin >> conditionRating; // Letter rating system prefered
-		cout << "Please enter the price you paid for the comic." << endl;
-		 cin >> comicPrice;
-		cout << endl;
+	do {
+		if (userAnswer == 'Y'){
+			cin.ignore(100,'\n');
+			cout << "Please enter series name." << endl;
+			getline(cin, seriesName);
+			cout << "Please enter issue name, if applicable." << endl;
+			getline(cin, issueName); // Can be considered a subtitle
+			cout << "Please enter author's name." << endl;
+			getline(cin, author);
+			cout << "Please enter the artist's or penciller's name." << endl;
+			getline(cin, artistOrPenciller); // Comics can say either
+			cout << "Please enter colorist's name." << endl;
+			getline(cin, colorist); // The artist usually only draws, not colors
+			cout << "Please enter issue number." << endl;
+			 cin >> issueNumber;
+			cout << "Please enter variant letter." << endl;
+			 cin >> variantLetter;
+			cout << "Please enter the publish date. Separate the month and year with a slash if necessary." << endl;
+			 cin >> publishDate; // string/getline didn't work.
+			cout << "Please enter condition rating. (Use letter notation.)" << endl;
+			 cin >> conditionRating; // Letter rating system prefered
+			cout << "Please enter the price you paid for the comic." << endl;
+			 cin >> comicPrice;
+			cout << endl;
 
-		cout << seriesName << "| " << issueName << "| " << author << 
-		"| " << artistOrPenciller  << "| " << colorist << "| " << issueNumber << variantLetter << "| " << 
-		publishDate << "| " << conditionRating << "| " << comicPrice << endl;
-		cout << endl;
+			cout << seriesName << "| " << issueName << "| " << author << 
+			"| " << artistOrPenciller  << "| " << colorist << "| " << issueNumber << variantLetter << "| " << 
+			publishDate << "| " << conditionRating << "| " << comicPrice << endl;
+			cout << endl;
 
-		cout << "Do you want to input more comics? Y (Yes) or N (No)" << endl;
-		 cin >> userAnswer;
-		}
-	else
-		cout << "Keep your comics safe! Thank you for using this program." << endl;
-	
+			cout << "Do you want to input more comics? Y (Yes) or N (No)" << endl;
+			 cin >> userAnswer;
+			}
+	 }
+	while (userAnswer == 'Y');
+	if (userAnswer == 'N'){
+		cout << endl;
+		cout << endl;
+		cout << "Keep your comics safe! Thank you for using this program." << endl;}
 
 return 0;
 }
